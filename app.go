b091e59af337cb5e2d26a3b85a41bb203cd0dc9b
Tnze <cjd001113@outlook.com>
2019-07-29 21:40:19 +0800

    添加setup脚本，自动安装cqcfg

--- app.go
+++ app.go
@@ -3,11 +3,11 @@ package main
 import "github.com/Tnze/CoolQ-Golang-SDK/cqp"
 
 //go:generate cqcfg .
-// cqp: 名称: 一个超棒的Go语言插件
+// cqp: 名称: GoDemo
 // cqp: 版本: 1.0.0:1
 // cqp: 作者: Tnze
-// cqp: 简介: 酷Q插件模板，你创作的起点
-func main() { /*空*/ }
+// cqp: 简介: 一个超棒的Go语言插件Demo，它会回复你的私聊消息~
+func main() { /*此处应当留空*/ }
 
 func init() {
 	cqp.AppID = "me.cqp.tnze.demo" // TODO: 修改为这个插件的ID
@@ -15,6 +15,6 @@ func init() {
 }
 
 func onPrivateMsg(subType, msgID int32, fromQQ int64, msg string, font int32) int32 {
-	cqp.SendPrivateMsg(fromQQ, msg)
+	cqp.SendPrivateMsg(fromQQ, msg) //复读机
 	return 0
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/Tnze/CoolQ-Golang-Plugin
 
 go 1.12
 
-require github.com/Tnze/CoolQ-Golang-SDK v0.0.0-20190723170611-44d95cc463db
+require github.com/Tnze/CoolQ-Golang-SDK v0.0.0-20190729083435-26b5706b9042
