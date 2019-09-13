e9280d71dbfb5d9b620dd1d5a733997b9d3a9eb2
Tnze <cjd001113@outlook.com>
2019-09-13 12:19:46 +0800

    build.bat全部改用英文，免得乱码

--- build.bat
+++ build.bat
@@ -1,26 +1,25 @@
-:: 关闭控制台回显
 @echo off
 
-echo 正在生成app.json
+echo Generating app.json
 go build github.com/Tnze/CoolQ-Golang-SDK/tools/cqcfg
 go generate
 IF ERRORLEVEL 1 pause
 
-echo 正在设置环境变量
+echo Setting env vars
 SET CGO_LDFLAGS=-Wl,--kill-at
 SET CGO_ENABLED=1
 SET GOOS=windows
 SET GOARCH=386
 SET GOPROXY=https://goproxy.cn
 
-echo 正在编译app.dll
+echo Building app.dll
 go build -buildmode=c-shared -o app.dll
 IF ERRORLEVEL 1 pause
 
-:: 如果设置了环境变量，则把app.dll和app.json复制到酷Q的dev文件夹
-REM SET DevDir=D:\酷Q Pro\dev\me.cqp.tnze.demo
+:: Copy app.dll amd app.json
+:: SET DevDir=D:\酷Q Pro\dev\me.cqp.tnze.demo
 if defined DevDir (
-    echo 正在复制文件
+    echo Coping files
     for %%f in (app.dll,app.json) do move %%f "%DevDir%\%%f" > nul
     IF ERRORLEVEL 1 pause
 )
